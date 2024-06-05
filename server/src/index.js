1b0693f6082a5c65d101216a65be17db5d38bb35
heyyrahul <rkkamat9213@gmail.com>
2024-06-05 23:47:49 +0530

    env error resolved

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { router } = require('./routes/formRoutes');
 
+require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.port || 5000;
 
 app.use('/api', router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
- 
+  
